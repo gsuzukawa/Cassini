3de8bec47a183154cc5e4be68322064573f39871
Behr Process <Behr@behrdevimac.local>
2017-06-21 12:19:42 -0700

    Adding initial resource

---
+++ Cassini/DemoURL.swift
@@ -0,0 +1,14 @@
+//
+//  DemoURL.swift
+//  Cassini
+//
+//  Created by Gregg on 6/21/17.
+//  Copyright © 2017 Suzukawa Solutions. All rights reserved.
+//
+
+import Foundation
+
+struct DemoURL {
+
+    
+}
